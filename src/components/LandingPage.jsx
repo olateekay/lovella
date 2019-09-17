a56ad46c01a0a6e619fd35a5bd1961c8cbcc3fad
Olatokunbo <Olatokunbo@Javas-MacBook-Pro.local>
2019-09-17 15:14:53 +0100

    installation of axios

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './FirstTextField.css'
 
 class LandingPage extends React.Component {
    render (){
        return(
            <div>
-              <h1 className='landing'>Lovella is working its magic!</h1> 
+              <h1 className='text'>Lovella is working its magic!</h1> 
            </div>
        );
    }
