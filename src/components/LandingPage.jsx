bdf2fabe4d207c921e8d09265418e7156086ec4f
Olatokunbo <Olatokunbo@Javas-MacBook-Pro.local>
2019-09-23 15:31:36 +0100

    solved the cors origin error,installed react spinner and worked on the results page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './FirstTextField.css'
 import axios from 'axios';
+import { css } from '@emotion/core';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 const API_URL = 'https://love-calculator.p.rapidapi.com/';
-const API_KEY = '4ca5c860b4msh5b19fe48080e408p1f6c49jsne9e041833357'
+const API_KEY = '4ca5c860b4msh5b19fe48080e408p1f6c49jsne9e041833357';
+const override = css`
+                display: block;
+                margin: 0 auto;
+                border-color: red;`;
+
+
 
 
 class LandingPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true
+        }
+    }
 
     componentDidMount() {
         this.handleGetPercentage()
@@ -27,7 +41,7 @@ class LandingPage extends React.Component {
         }
 
         const url = `${API_URL}getPercentage`;
-        axios.get(url, {headers: settings , params: param})
+        axios.get(url, { headers: settings, params: param })
             .then(res => {
                 console.log(res);
                 console.log(res.data);
@@ -39,6 +53,15 @@ class LandingPage extends React.Component {
         return (
             <div>
                 <h1 className='text'>Lovella is working its magic!</h1>
+                <div className='sweet-loading'>
+                    <ClipLoader
+                        css={override}
+                        sizeUnit={"px"}
+                        size={150}
+                        color={'#123abc'}
+                        loading={this.state.loading}
+                    />
+                </div>
             </div>
         );
     }
